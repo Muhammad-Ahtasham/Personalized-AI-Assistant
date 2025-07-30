6f937e3f66feb3e884cd2491ad57d9eb47dedcda
Muhammad-Ahtasham <ahtashamahsan988@gmail.com>
2025-07-30 15:20:41 +0500

    PASA

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+
 import { useEffect, useState } from "react";
 import { useUser } from "@clerk/nextjs";
 // import { redirect } from "next/dist/server/api-utils";
